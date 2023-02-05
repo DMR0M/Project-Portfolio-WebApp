f9fc941abf31e0b8f1de956c14557f8b7657a28d
Rommel <rommeldm87@gmail.com>
2023-02-05 22:57:25 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+col_1, col_2 = st.columns(2)
+
+with col_1:
+    st.image("images/Mittens.png")
+
+with col_2:
+    st.title("Rommel Rudolf A. Dela Merced")
+    content = """
+        An Aspiring Python Developer
+    """
+    st.write(content)
+
